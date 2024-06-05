571d20f63363893df621b3f3d6765cecb4243835
Anshu <31213329+Iamanshup@users.noreply.github.com>
2024-06-05 19:30:03 +0530

    Time: 39 ms (51.44%) | Memory: 16.6 MB (37.83%) - LeetSync

---
+++ 873-guess-the-word/guess-the-word.py
@@ -0,0 +1,24 @@
+# """
+# This is Master's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+# class Master:
+#     def guess(self, word: str) -> int:
+
+
+from random import random
+
+
+class Solution:
+    def findSecretWord(self, words: List[str], master: "Master") -> None:
+        def get_match(w1, w2):
+            return sum([1 if w1[i] == w2[i] else 0 for i in range(6)])
+
+        while words:
+            word = words[int(random() * len(words))]
+            x = master.guess(word)
+            if x == 6:
+                return word
+            words = [word2 for word2 in words if get_match(word, word2) == x]
+
+        return None
