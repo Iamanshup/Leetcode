6ee815284ab3ed7c929d34af096dc3663e3fcb9a
Anshu <31213329+Iamanshup@users.noreply.github.com>
2024-06-09 13:51:34 +0530

    Time: 48 ms (9.18%) | Memory: 10.9 MB (5.26%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    bool canMakeValidSubstring(int substringSize, string s, int k) {
+        vector<int> freq(26);
+        int n = s.size();
+
+        for (int i = 0; i < n; ++i) {
+            freq[s[i] - 'A']++;
+
+            if (i >= substringSize) {
+                freq[s[i - substringSize] - 'A']--;
+            }
+
+            int mx = 0;
+            for (int f : freq) {
+                mx = max(mx, f);
+            }
+
+            if (substringSize - mx <= k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int low = 1, high = n+1;
+
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (canMakeValidSubstring(mid, s, k)) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
